b5833ccb00b202a53a3bd7fb0ba13cd78d67a0dd
inkh <jhwe92@gmail.com>
2018-10-28 22:14:01 -0700

    added data annotations

--- AsyncInn/AsyncInn/Models/HotelRoom.cs
+++ AsyncInn/AsyncInn/Models/HotelRoom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,16 @@ namespace AsyncInn.Models
     public class HotelRoom
     {
         public int HotelID { get; set; }
+
+        [Required]
         public int RoomNumber { get; set; }
+
         public decimal RoomID { get; set; }
+
+        [Required]
         public decimal Rate { get; set; }
+
+        [Required]
         public bool PetFriendly { get; set; }
 
         //Nav Props
--- AsyncInn/AsyncInn/Models/Room.cs
+++ AsyncInn/AsyncInn/Models/Room.cs
@@ -9,8 +9,12 @@ namespace AsyncInn.Models
     public class Room
     {
         public int ID { get; set; }
+
+        [Required]
         [Display(Name = "Room Name")]
         public string Name { get; set; }
+
+        [Required]
         [EnumDataType(typeof(Layout))]
         public Layout Layout { get; set; }
 
