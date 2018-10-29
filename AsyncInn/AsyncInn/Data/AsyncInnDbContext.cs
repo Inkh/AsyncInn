0d02fb448798b3d0150283db949d03e102822082
inkh <jhwe92@gmail.com>
2018-10-28 22:27:53 -0700

    seeded amenities data

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -24,6 +24,35 @@ namespace AsyncInn.Data
             modelBuilder.Entity<HotelRoom>().HasKey(
                 ce => new { ce.RoomID, ce.HotelID }
                 );
+
+            modelBuilder.Entity<Amenities>().HasData(
+                new Amenities
+                {
+                    ID = 1,
+                    Name = "Air Conditioning"
+                },
+                new Amenities
+                {
+                    ID = 2,
+                    Name = "Private Parking"
+                },
+                new Amenities
+                {
+                    ID = 3,
+                    Name = "Heater"
+                },
+                new Amenities
+                {
+                    ID = 4,
+                    Name = "In-unit washer dryer"
+                },
+                new Amenities
+                {
+                    ID = 5,
+                    Name = "Jacuzzi"
+                }
+                );
+
         }
 
         public DbSet<Room> Room { get; set; }
--- AsyncInn/AsyncInn/Migrations/20181029052703_seededAmenityData.Designer.cs
+++ AsyncInn/AsyncInn/Migrations/20181029052703_seededAmenityData.Designer.cs
@@ -4,14 +4,16 @@ using AsyncInn.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AsyncInn.Migrations
 {
     [DbContext(typeof(AsyncInnDbContext))]
-    partial class AsyncInnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181029052703_seededAmenityData")]
+    partial class seededAmenityData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
